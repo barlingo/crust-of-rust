5177047c54fa09804caffc8f7d13e040476b65a4
barlingo <16936512+barlingo@users.noreply.github.com>
2022-05-24 21:11:08 -0400

    [conf] configuring git correctly

--- channels-panama/target/debug/deps/channels-833bae0699f45f5c.d
+++ channels-panama/target/debug/deps/channels-833bae0699f45f5c.d
@@ -1,5 +1,5 @@
-/home/diegob/Git/rust-learn/crust-of-rust/channels/target/debug/deps/channels-833bae0699f45f5c.rmeta: src/lib.rs
+/home/diegob/Git/rust-learn/crust-of-rust/channels-panama/target/debug/deps/channels-833bae0699f45f5c.rmeta: src/lib.rs
 
-/home/diegob/Git/rust-learn/crust-of-rust/channels/target/debug/deps/channels-833bae0699f45f5c.d: src/lib.rs
+/home/diegob/Git/rust-learn/crust-of-rust/channels-panama/target/debug/deps/channels-833bae0699f45f5c.d: src/lib.rs
 
 src/lib.rs:
